ebc8a09c189951ba874211a4cb551b45771fe8ec
ox404fff <ox404fff@gmail.com>
2021-01-04 21:01:46 +0100

    new file:   conf.d/personal.conf 	new file:   run.sh

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env /bin/bash
+
+export APPLICATION=$1
+export VERSION=""
+export NAME=""
+
+export IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.Networks.frontend.IPAddress }}' ${APPLICATION})
+
+echo $IP_ADDRESS
+
+# Parse:
+for i in ${APPLICATION//-/ }
+do
+    VERSION=$i
+    [[ -z "${NAME// }" ]] && NAME=$i || NAME="${NAME}-$i"
+done
+
+NAME=${NAME%"-$VERSION"}
+
+# Register in LB
+docker cp /var/www/nginx/conf/${NAME}.conf nginx:/etc/nginx/conf.d
+docker exec nginx sed -i "s/---IP_ADDRESS---/${IP_ADDRESS}/" /etc/nginx/conf.d/${NAME}.conf
+docker exec nginx nginx -s reload
+
+
+# Stop old containers
+for name in $(docker ps --format '{{.Names}}' | grep ^${NAME}-[0-9]*$)
+do
+    if [[ "$name" != "${NAME}-${VERSION}" ]]
+    then
+        docker stop ${name}
+    fi
+done
+
