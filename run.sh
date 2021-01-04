b077695c93c998589cd6466c149677c6c9a48624
ox404fff <ox404fff@gmail.com>
2021-01-05 00:24:44 +0100

    test

--- run.sh
+++ run.sh
@@ -6,10 +6,17 @@ export INSTANCE_NAME="${NAME}-${VERSION}"
 
 export IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.Networks.frontend.IPAddress }}' ${INSTANCE_NAME})
 
+docker build -t doctl -f ${WORKSPACE_PATH}/Dockerfile ${WORKSPACE_PATH}
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
+	-e DIGITALOCEAN_ACCESS_TOKEN=${DIGITALOCEAN_ACCESS_TOKEN} \
+        -e APP_NAME=${APP_NAME} \
+        -e APP_VERSION=${APP_VERSION} \
+        doctl /run/run.sh 
+
 echo "Ip address of new instance: ${IP_ADDRESS}"
 
 # Register in LB
-docker cp ./conf.d/${NAME}.conf nginx:/etc/nginx/conf.d
+docker cp ${WORKSPACE_PATH}/conf.d/${NAME}.conf nginx:/etc/nginx/conf.d
 docker exec nginx sed -i "s/---IP_ADDRESS---/${IP_ADDRESS}/" /etc/nginx/conf.d/${NAME}.conf
 docker exec nginx nginx -s reload
 
